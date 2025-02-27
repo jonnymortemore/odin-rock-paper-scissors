d4a4b407c5a0662694c520276c67f6a331bf262c
Jonny Mortemore <jonathanmortemore@gmail.com>
2025-02-27 15:09:34 +1100

    Finished initial project outcomes

--- code.js
+++ code.js
@@ -1,5 +1,12 @@
-console.log("hello!");
+const SCISSORS = "scissors"
+const ROCK = "rock"
+const PAPER = "paper"
+let humanScore = 0;
+let computerScore = 0;
 
+
+console.log("Play Rock, Paper, Scissors!")
+playGame()
 
 function getComputerChoice() {
     let value = Math.random();
@@ -23,7 +30,7 @@ function getUserChoice() {
         choice = window.prompt("Pick rock, paper, scissors!");
         choice.trim();
         choice.toLowerCase();
-        if (["scissors", "rock", "paper"].includes(choice)) {
+        if ([ROCK, PAPER, SCISSORS].includes(choice)) {
             choiceMade = true;
         }
         else {
@@ -34,4 +41,70 @@ function getUserChoice() {
     }
     
     return choice;
+}
+
+function playerRound(playerMove, computerMove) {
+    let winner = true;
+    if (playerMove === computerMove) {
+        console.log(`Draw! Computer also played ${playerMove}`);
+        return
+    }
+    else if (playerMove === ROCK) {
+        if (computerMove === SCISSORS) {
+            humanScore++;
+            winner = true;
+        }
+        else {
+            computerScore++;
+            winner = false;
+            
+        }
+    }
+    else if (playerMove === PAPER) {
+        if (computerMove === ROCK) {
+            humanScore++;
+            winner = true;
+        }
+        else {
+            computerScore++
+            winner = false;
+        }
+    }
+    else if (playerMove === SCISSORS) {
+        if (computerMove === PAPER) {
+            humanScore++;
+            winner = true;
+        }
+        else {
+            computerScore++;
+            winner = false;
+        }
+    }
+
+    if (winner) {
+        console.log(`You win! ${playerMove} beats ${computerMove}`);
+    } else {
+        console.log(`You lose! ${computerMove} beats ${playerMove}`);
+    }
+}
+
+function playGame() {
+    const roundTotal = 5;
+
+    for (let round = 1; round <= roundTotal; round++) {
+        console.log("Round " + round)
+        playerRound(getUserChoice(), getComputerChoice())
+        console.log(`You: ${humanScore}`)
+        console.log(`Computer: ${computerScore}`)
+    }
+
+    if (humanScore > computerScore) {
+        console.log("You win!")
+    }
+    else if (humanScore == computerScore) {
+        console.log("It's a draw!")
+    }
+    else {
+        console.log("Damn you lost!")
+    }
 }
